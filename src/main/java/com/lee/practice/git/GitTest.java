c934adf508ce211741ca9e2c639c45ec02b861b9
g19871230 <g19871230@163.com>
2019-12-03 16:35:58 +0800

    new git

---
+++ src/main/java/com/lee/practice/git/GitTest.java
@@ -0,0 +1,21 @@
+package com.lee.practice.git;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Date 2019/12/3
+ * @Create by lixing
+ */
+@RestController
+@EnableAutoConfiguration
+public class GitTest {
+
+    @RequestMapping("/")
+    public String home() {
+        return "hi git!";
+    }
+
+}
